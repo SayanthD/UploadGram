773930f5374f02e4f7591aa93a6a3229a1bc00c4
Sayanth <13906889+SayanthD@users.noreply.github.com>
2025-04-06 12:04:35 +0530

    Update uploadgram.py

--- uploadgram/uploadgram.py
+++ uploadgram/uploadgram.py
@@ -20,7 +20,7 @@ from .config import get_config
 
 # Configure logging
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.WARNING,
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
